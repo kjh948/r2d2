8d7ae9c22c147c3a91bbc5bddfcd26fd9083f8b1
kjh948@gmail.com <kjh948@gmail.com>
2022-03-05 22:01:33 +0900

    something

--- set.sh
+++ set.sh
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export ROS_MASTER_URI=http://192.168.1.224:11311
 #export ROS_MASTER_URI=http://r2d2.local:11311
 export ROS_HOSTNAME=192.168.1.224
 #export ROS_IP=r2d2.local
-=======
-export ROS_MASTER_URI=http://192.168.29.162:11311
-export ROS_HOSTNAME=192.168.29.162
-export ROS_IP=192.168.29.162
->>>>>>> 2ba3a47315693fa8d4e5848b50e0cafee52bd938
